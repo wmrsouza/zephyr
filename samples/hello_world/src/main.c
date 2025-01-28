c7f662811c28598232e1bdb7cfb532072f7b9f6a
Marcio Ribeiro <marcio.ribeiro@espressif.com>
2025-01-28 15:08:54 -0300

    __attribute__ ((section (".ext_ram.text"))) check code

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,19 @@
  */
 
 #include <stdio.h>
+#include <inttypes.h>
+#include <esp_attr.h>
+
+/* __attribute__ ((section (".ext_ram.text"))) void hello_from_psram(void) */
+EXT_RAM_NOINIT_ATTR void hello_from_psram(void)
+{
+	printf("hello_from_psram: 0x%"PRIX32"\n\r", (uint32_t)hello_from_psram);
+	printf("Hello World (from PSRAM)! %s\n", CONFIG_BOARD_TARGET);
+}
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	hello_from_psram();
 
 	return 0;
 }
